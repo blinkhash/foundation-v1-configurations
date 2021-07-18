56a270778caa6b7ff4946762e85139c728c33da9
Blinkhash <blinkhashpool@gmail.com>
2021-07-18 14:55:54 -0400

    Add working litecoin configuration, small changes

---
+++ README.md
@@ -0,0 +1,29 @@
+# Blinkhash Configurations
+
+[![License: GPL v2](https://img.shields.io/badge/License-GPL%20v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)
+[![Discord](https://img.shields.io/discord/738590795384356904)](https://discord.gg/8xtHZFKJQY)
+
+This repository is a collection of coin configuration files confirmed to be working with Blinkhash's mining platform. To request for a configuration file to be added to the repository, open an issue or pull request and it will be addressed
+
+#### Need Support?
+
+If you need help with a code-related matter, the first place to look is our [Discord](https://discord.gg/8xtHZFKJQY), where the developers will be available to answer any questions. However, please do not come to me with issues regarding setup. Use Google and the existing documentation for that.
+
+---
+
+### Donations
+
+Maintaining this project has always been driven out of nothing more than a desire to give back to the mining community, however I always appreciate donations, especially if this repository helps you in any way.
+
+- Bitcoin: 3EbrVYLxN5WeQmPpL6owo3A7rJELXecbbc
+- Ethereum: 0xd3e3daED621d228244Fa89A3dd8AF07B52E72319
+- Litecoin: MFWpARrSADAy3Qj79C4pSasS9F156QipwC
+- ZCash: t1NSk8gyiou8TxWRZTVuUkfM5f9riopN58A
+
+---
+
+### License
+
+Released under the GNU General Public License v2. See http://www.gnu.org/licenses/gpl-2.0.html for more information
+
+---
---
+++ configurations/litecoin.js
@@ -0,0 +1,124 @@
+// Litecoin Configuration
+const config = {};
+
+// Main Configuration
+config.enabled = true;
+config.featured = true;
+config.address = '[address]';
+config.debug = false;
+config.identifier = 'https://github.com/blinkhash/blinkhash-server';
+config.logo = '[logo]';
+
+// Coin Configuration
+config.coin = {};
+config.coin.name = 'Litecoin';
+config.coin.symbol = 'LTC';
+config.coin.asicBoost = false;
+config.coin.getInfo = false;
+config.coin.segwit = true;
+config.coin.rewards = '';
+
+// Algorithm Configuration
+config.coin.algorithms = {};
+config.coin.algorithms.mining = 'scrypt';
+config.coin.algorithms.block = 'sha256d';
+config.coin.algorithms.coinbase = 'sha256d';
+
+// Mainnet Configuration
+config.coin.mainnet = {};
+config.coin.mainnet.bech32 = 'ltc';
+config.coin.mainnet.bip32 = {};
+config.coin.mainnet.bip32.public = Buffer.from('0488B21E', 'hex').readUInt32LE(0);
+config.coin.mainnet.bip32.private = Buffer.from('0488ADE4', 'hex').readUInt32LE(0);
+config.coin.mainnet.peerMagic = 'fbc0b6db';
+config.coin.mainnet.pubKeyHash = Buffer.from('30', 'hex').readUInt8(0);
+config.coin.mainnet.scriptHash = Buffer.from('32', 'hex').readUInt8(0);
+config.coin.mainnet.wif = Buffer.from('b0', 'hex').readUInt8(0);
+config.coin.mainnet.coin = 'ltc';
+
+// Mainnet Configuration
+config.coin.testnet = {};
+config.coin.testnet.bech32 = 'tltc';
+config.coin.testnet.bip32 = {};
+config.coin.testnet.bip32.public = Buffer.from('043587CF', 'hex').readUInt32LE(0);
+config.coin.testnet.bip32.private = Buffer.from('04358394', 'hex').readUInt32LE(0);
+config.coin.testnet.peerMagic = 'fcc1b7dc';
+config.coin.testnet.pubKeyHash = Buffer.from('6F', 'hex').readUInt8(0);
+config.coin.testnet.scriptHash = Buffer.from('3A', 'hex').readUInt8(0);
+config.coin.testnet.wif = Buffer.from('EF', 'hex').readUInt8(0);
+config.coin.testnet.coin = 'ltc';
+
+// Daemon Configuration
+config.daemons = [];
+
+const daemons1 = {};
+daemons1.host = '[host]';
+daemons1.port = [port];
+daemons1.username = '[username]';
+daemons1.password = '[password]';
+config.daemons.push(daemons1);
+
+// Payment Configuration
+config.payments = {};
+config.payments.enabled = true;
+config.payments.checkInterval = 20;
+config.payments.paymentInterval = 7200;
+config.payments.minConfirmations = 10;
+config.payments.minPayment = 0.005;
+config.payments.transactionFee = 0.0004;
+config.payments.daemon = {};
+config.payments.daemon.host = '[host]';
+config.payments.daemon.port = [port];
+config.payments.daemon.username = '[username]';
+config.payments.daemon.password = '[password]';
+
+// Banning Configuration
+config.banning = {};
+config.banning.time = 600;
+config.banning.invalidPercent = 0.5;
+config.banning.checkThreshold = 500;
+config.banning.purgeInterval = 300;
+
+// Port Configuration
+config.ports = [];
+
+const ports1 = {};
+ports1.port = [port];
+ports1.enabled = true;
+ports1.type = [type];
+ports1.difficulty = {};
+ports1.difficulty.initial = [initial];
+ports1.difficulty.minimum = [minimum];
+ports1.difficulty.maximum = [maximum];
+ports1.difficulty.targetTime = 15;
+ports1.difficulty.retargetTime = 90;
+ports1.difficulty.variance = 0.3;
+config.ports.push(ports1);
+
+// Recipients Configuration
+config.recipients = [];
+
+const recipient1 = {};
+recipient1.address = '[address]';
+recipient1.percentage = [percentage];
+config.recipients.push(recipient1);
+
+// P2P Configuration
+config.p2p = {};
+config.p2p.enabled = true;
+config.p2p.host = '[host]';
+config.p2p.port = [port];
+config.p2p.disableTransactions = true;
+
+// Settings Configuration
+config.settings = {};
+config.settings.blockRefreshInterval = 1000;
+config.settings.connectionTimeout = 600;
+config.settings.emitInvalidBlockHashes = false;
+config.settings.hashrateWindow = 300;
+config.settings.jobRebroadcastTimeout = 60;
+config.settings.tcpProxyProtocol = false;
+config.settings.validateWorkerUsername = true;
+
+// Export Configuration
+module.exports = config;
