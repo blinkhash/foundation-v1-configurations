929d5a707c1cc0f772018f3d0936766fddef5b39
Blinkhash <blinkhashpool@gmail.com>
2021-08-05 12:11:46 -0400

    Fix configurations to work with new build

--- configurations/dash.js
+++ configurations/dash.js
@@ -1,81 +1,25 @@
-// Dash Configuration
-const config = {};
+/*
+ *
+ * Example (Dash)
+ *
+ */
 
 // Main Configuration
+////////////////////////////////////////////////////////////////////////////////
+
+// Miscellaneous Configuration
+const config = {};
 config.enabled = true;
-config.featured = true;
-config.address = '[address]';
-config.debug = false;
-config.identifier = 'https://github.com/blinkhash/foundation-server';
-config.logo = '[logo]';
 
-// Coin Configuration
-config.coin = {};
-config.coin.name = 'Dash';
-config.coin.symbol = 'DASH';
-config.coin.asicBoost = false;
-config.coin.getInfo = false;
-config.coin.segwit = true;
-config.coin.rewards = '';
-
-// Algorithm Configuration
-config.coin.algorithms = {};
-config.coin.algorithms.mining = 'x11';
-config.coin.algorithms.block = 'x11';
-config.coin.algorithms.coinbase = 'sha256d';
-
-// Mainnet Configuration
-config.coin.mainnet = {};
-config.coin.mainnet.bech32 = '';
-config.coin.mainnet.bip32 = {};
-config.coin.mainnet.bip32.public = Buffer.from('0488B21E', 'hex').readUInt32LE(0);
-config.coin.mainnet.bip32.private = Buffer.from('0488ADE4', 'hex').readUInt32LE(0);
-config.coin.mainnet.peerMagic = 'bf0c6bbd';
-config.coin.mainnet.pubKeyHash = Buffer.from('4C', 'hex').readUInt8(0);
-config.coin.mainnet.scriptHash = Buffer.from('10', 'hex').readUInt8(0);
-config.coin.mainnet.wif = Buffer.from('cc', 'hex').readUInt8(0);
-config.coin.mainnet.coin = 'dash';
-
-// Testnet Configuration
-config.coin.testnet = {};
-config.coin.testnet.bech32 = '';
-config.coin.testnet.bip32 = {};
-config.coin.testnet.bip32.public = Buffer.from('043587CF', 'hex').readUInt32LE(0);
-config.coin.testnet.bip32.private = Buffer.from('04358394', 'hex').readUInt32LE(0);
-config.coin.testnet.peerMagic = 'cee2caff';
-config.coin.testnet.pubKeyHash = Buffer.from('8C', 'hex').readUInt8(0);
-config.coin.testnet.scriptHash = Buffer.from('13', 'hex').readUInt8(0);
-config.coin.testnet.wif = Buffer.from('EF', 'hex').readUInt8(0);
-config.coin.testnet.coin = 'dash';
-
-// Daemon Configuration
-config.daemons = [];
-
-const daemons1 = {};
-daemons1.host = '[host]';
-daemons1.port = [port];
-daemons1.username = '[username]';
-daemons1.password = '[password]';
-config.daemons.push(daemons1);
-
-// Payment Configuration
-config.payments = {};
-config.payments.enabled = true;
-config.payments.checkInterval = 20;
-config.payments.paymentInterval = 7200;
-config.payments.minConfirmations = 10;
-config.payments.minPayment = 0.005;
-config.payments.transactionFee = 0.0004;
-config.payments.daemon = {};
-config.payments.daemon.host = '[host]';
-config.payments.daemon.port = [port];
-config.payments.daemon.username = '[username]';
-config.payments.daemon.password = '[password]';
+// Statistics Configuration
+config.name = '[name]';
+config.coins = ['Dash'];
+config.logo = '';
 
 // Banning Configuration
 config.banning = {};
 config.banning.time = 600;
-config.banning.invalidPercent = 0.5;
+config.banning.invalidPercent = 50;
 config.banning.checkThreshold = 500;
 config.banning.purgeInterval = 300;
 
@@ -95,14 +39,6 @@ ports1.difficulty.retargetTime = 90;
 ports1.difficulty.variance = 0.3;
 config.ports.push(ports1);
 
-// Recipients Configuration
-config.recipients = [];
-
-const recipient1 = {};
-recipient1.address = '[address]';
-recipient1.percentage = [percentage];
-config.recipients.push(recipient1);
-
 // P2P Configuration
 config.p2p = {};
 config.p2p.enabled = true;
@@ -113,10 +49,89 @@ config.p2p.port = [port];
 config.settings = {};
 config.settings.blockRefreshInterval = 1000;
 config.settings.connectionTimeout = 600;
-config.settings.emitInvalidBlockHashes = false;
 config.settings.hashrateWindow = 300;
 config.settings.jobRebroadcastTimeout = 60;
 config.settings.tcpProxyProtocol = false;
+
+// Primary Configuration
+////////////////////////////////////////////////////////////////////////////////
+
+// Miscellaneous Configuration
+config.primary = {};
+config.primary.address = '[address]';
+
+// Coin Configuration
+config.primary.coin = {};
+config.primary.coin.name = 'Dash';
+config.primary.coin.symbol = 'DASH';
+config.primary.coin.asicBoost = false;
+config.primary.coin.getInfo = false;
+config.primary.coin.messages = false;
+config.primary.coin.segwit = true;
+config.primary.coin.staking = false;
+config.primary.coin.rewards = '';
+
+// Algorithm Configuration
+config.primary.coin.algorithms = {};
+config.primary.coin.algorithms.mining = 'x11';
+config.primary.coin.algorithms.block = 'x11';
+config.primary.coin.algorithms.coinbase = 'sha256d';
+
+// Mainnet Configuration
+config.primary.coin.mainnet = {};
+config.primary.coin.mainnet.bech32 = '';
+config.primary.coin.mainnet.bip32 = {};
+config.primary.coin.mainnet.bip32.public = Buffer.from('0488B21E', 'hex').readUInt32LE(0);
+config.primary.coin.mainnet.bip32.private = Buffer.from('0488ADE4', 'hex').readUInt32LE(0);
+config.primary.coin.mainnet.peerMagic = 'bf0c6bbd';
+config.primary.coin.mainnet.pubKeyHash = Buffer.from('4C', 'hex').readUInt8(0);
+config.primary.coin.mainnet.scriptHash = Buffer.from('10', 'hex').readUInt8(0);
+config.primary.coin.mainnet.wif = Buffer.from('cc', 'hex').readUInt8(0);
+config.primary.coin.mainnet.coin = 'dash';
+
+// Testnet Configuration
+config.primary.coin.testnet = {};
+config.primary.coin.testnet.bech32 = '';
+config.primary.coin.testnet.bip32 = {};
+config.primary.coin.testnet.bip32.public = Buffer.from('043587CF', 'hex').readUInt32LE(0);
+config.primary.coin.testnet.bip32.private = Buffer.from('04358394', 'hex').readUInt32LE(0);
+config.primary.coin.testnet.peerMagic = 'cee2caff';
+config.primary.coin.testnet.pubKeyHash = Buffer.from('8C', 'hex').readUInt8(0);
+config.primary.coin.testnet.scriptHash = Buffer.from('13', 'hex').readUInt8(0);
+config.primary.coin.testnet.wif = Buffer.from('EF', 'hex').readUInt8(0);
+config.primary.coin.testnet.coin = 'dash';
+
+// Daemon Configuration
+config.primary.daemons = [];
+
+const daemons1 = {};
+daemons1.host = '[host]';
+daemons1.port = [port];
+daemons1.username = '[username]';
+daemons1.password = '[password]';
+config.primary.daemons.push(daemons1);
+
+// Payment Configuration
+config.primary.payments = {};
+config.primary.payments.enabled = true;
+config.primary.payments.checkInterval = 20;
+config.primary.payments.paymentInterval = 7200;
+config.primary.payments.minConfirmations = 10;
+config.primary.payments.minPayment = 0.005;
+config.primary.payments.transactionFee = 0.0004;
+config.primary.payments.daemon = {};
+config.primary.payments.daemon.host = '[host]';
+config.primary.payments.daemon.port = [port];
+config.primary.payments.daemon.username = '[username]';
+config.primary.payments.daemon.password = '[password]';
+
+// Recipients Configuration
+config.primary.recipients = [];
+
+const recipient1 = {};
+recipient1.address = '[address]';
+recipient1.percentage = [percentage];
+config.primary.recipients.push(recipient1);
 
 // Export Configuration
 module.exports = config;
